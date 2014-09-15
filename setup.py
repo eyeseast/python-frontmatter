51e29d06c785a79cfe3a6df73f3ac89107bf39ec
chrisamico <eyeseast@gmail.com>
2014-09-14 22:37:39 -0400

    installer

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+
+requirements = [
+    'PyYAML',
+]
+
+
+setup(
+    name = 'python-frontmatter',
+    version = '0.1.0',
+    description = 'Parse and manage posts with YAML frontmatter',
+    long_description = readme,
+    author = 'Chris Amico',
+    author_email = 'eyeseast@gmail.com',
+    url = 'https://github.com/eyeseast/python-frontmatter',
+    py_modules = ['frontmatter'],
+    include_package_data = True,
+    install_requires = requirements,
+    license = 'MIT',
+    zip_safe = False,
+    keywords = 'frontmatter',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        #'Programming Language :: Python :: 3', # todo
+        #'Programming Language :: Python :: 3.3',
+        #'Programming Language :: Python :: 3.4',
+    ],
+    test_suite='test',
+)
