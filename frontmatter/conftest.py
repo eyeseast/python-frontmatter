cef1b91551814c003e1fc392ad368cd756f409d8
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2023-11-15 00:38:49 +0530

    add types to conftest

--- frontmatter/conftest.py
+++ frontmatter/conftest.py
@@ -2,7 +2,7 @@ import pytest
 
 
 @pytest.fixture(autouse=True)
-def add_globals(doctest_namespace):
+def add_globals(doctest_namespace: dict[str, object]) -> None:
     import frontmatter
 
     doctest_namespace["frontmatter"] = frontmatter
